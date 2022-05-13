b1f18f235fea2200f01774b973203a874bc933f5
alecnazworth <alecnazworth@icloud.com>
2022-05-12 22:39:40 -0400

    Added a PIN number system

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,39 @@ import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Enter 4 digit PIN: ");
+        int userPin = 1234;
+        Scanner pin = new Scanner(System.in);
+        int inputPin = pin.nextInt();
+
         System.out.println("Welcome! Deposit funds to get started.");
         boolean active = true;
-        while (active) {
+        while (active)
+        if (inputPin == userPin) {
             System.out.println("1. Deposit");
             System.out.println("2. Withdraw");
             System.out.println("3. Current Balance");
             System.out.println("4. Quit");
 
-            Scanner input = new Scanner(System.in);
-            double action = input.nextDouble();
-
-            if (action == 1) {
-                deposit();
+            Scanner inputAction = new Scanner(System.in);
+            double action = inputAction.nextDouble();
+                if (action == 1) {
+                    deposit();
+                }
+                if (action == 2) {
+                    withdraw();
+                }
+                if (action == 3) {
+                    balance();
+                }
+                if (action == 4) {
+                    System.out.println("Goodbye!");
+                    active = false;
+                }
             }
-            if (action == 2) {
-                withdraw();
-            }
-            if (action == 3) {
-                balance();
-            }
-            if (action == 4) {
-                System.out.println("Goodbye!");
-                active = false;
-            }
+        else {
+            System.out.println("Invalid pin. Try again.");
+            active = false;
         }
     }
     static private double accountBalance = 0;
